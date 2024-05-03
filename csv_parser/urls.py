b0f7ffecac4adf44df206bcf7f073ebc1162ffe9
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-05-03 22:39:47 +0300

    update: add path for parse_csv function

--- csv_parser/urls.py
+++ csv_parser/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
 
+    # PARSER
+    path("parse", views.parse_csv),
+
     # QUERIES
     path("all", views.get_all_records),
     path("towns", views.get_towns),
