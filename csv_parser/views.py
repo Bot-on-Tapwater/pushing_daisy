5909b5696937258a45e62e71c231a3158a93bb84
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-05-03 18:58:18 +0300

    feat: implement logic to query all records

--- csv_parser/views.py
+++ csv_parser/views.py
@@ -1,3 +1,97 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import csv
+from django.http import HttpResponseRedirect
+import os
+from django.conf import settings
+from .models import Sponsors
+
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from urllib.parse import quote_plus, urlencode
+import json
+from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseServerError, Http404
+from django.utils.datastructures import MultiValueDictKeyError
+from django.core.exceptions import ValidationError, MultipleObjectsReturned, PermissionDenied
+from django.http import QueryDict
+from django.db.models import F, ExpressionWrapper, fields, Sum
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage
+from django.core.serializers import serialize
+from django.forms.models import model_to_dict
+
+"""HELPER FUNCTIONS"""
+def parse_csv(request):
+    csv_file_path = os.path.join(settings.BASE_DIR, 'UkTiersponsors_All.csv')
+
+    # Check if the file exists
+    if os.path.exists(csv_file_path):
+        # Open and read the CSV file
+        with open(csv_file_path, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                new_record = Sponsors(organisation_name=row['Organisation Name'], town=row['Town'], industry=row['Industry'], main_tier=row['Main Tier'], sub_tier=row['Sub Tier'], date_added=row['Date Added'])
+
+                new_record.save()
+
+        return JsonResponse({'message': 'success'})
+    else:
+        return JsonResponse({'error': 'file not found'})
 
 # Create your views here.
+def index(request):
+    return JsonResponse({'message': "App is working"})
+
+
+"""PAGINATION"""
+def paginate_results(request, query_results, view_url, items_per_page=100):
+    items_per_page = items_per_page
+
+    page_number = request.GET.get('page', 1)
+
+    paginator = Paginator(query_results, items_per_page)
+
+    try:
+        page_obj = paginator.get_page(page_number)
+    
+    except EmptyPage:
+        return JsonResponse({"error": "Page not found"}, status=404)
+    
+    items_on_current_page = page_obj.object_list
+
+    json_data = {
+        'current_page': page_obj.number,
+        'total_pages': paginator.num_pages,
+        'query_results': [item.to_dict() for item in items_on_current_page],
+    }
+    
+    if page_obj.has_previous():
+        if "page=" in view_url:
+            json_data['previous_page'] = f"{view_url[:view_url.rfind('page=')]}page={page_obj.previous_page_number()}"
+        else:
+            json_data['previous_page'] = f"{view_url}?page={page_obj.previous_page_number()}"
+
+    if page_obj.has_next():
+        if "page=" in view_url:
+            json_data['next_page'] = f"{view_url[:view_url.rfind('page=')]}page={page_obj.next_page_number()}"
+        else:
+            json_data['next_page'] = f"{view_url}?page={page_obj.next_page_number()}"
+    
+    return json_data
+
+"""QUERIES"""
+
+def get_all_records(request):
+    view_url = request.build_absolute_uri()
+    all_records = Sponsors.objects.all()
+
+    # return JsonResponse([record.to_dict() for record in all_records], safe=False)
+    return JsonResponse(paginate_results(request, all_records, view_url), safe=False)
+
